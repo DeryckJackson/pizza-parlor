3308fa6a3a2a42e50f83060b8d41968dc8d60803
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-19 12:52:59 -0700

    collect values from form and save in pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 function Pizza(name, size) {
   this.name = name;
   this.size = size;
-  this.toppings = [];
+  this.toppings = [
+    { "pepperoni": 1 },
+    { "sausage": 1 },
+    { "pineapple": 2 },
+    { "canadianBacon": 1 },
+    { "redBellPeppers": 1},
+    { "hamburger": 1 },
+    { "olives": 1 },
+    { "onions": 1 },
+    { "greenPeppers": 1 }
+  ];
+  this.selectedToppings = [];
 }
 
 
 
 // UI logic
 $(document).ready(function() {
-  let pizza = new Pizza("Deryck", "large");
-  pizza.toppings.push("cheese");
-  console.log(pizza)
+  $("form#pizzaForm").submit(function() {
+    event.preventDefault();
+    let pizza = new Pizza("", "");
+    pizza.name = $("input#name").val()
+    pizza.size = $("input:radio[name=size]:checked").val()
+
+    $("input:checkbox[name=toppings]").each(function() {
+      if (this.checked) {
+      pizza.selectedToppings.push($(this).val());
+      }
+    });
+    console.log(pizza);
+  });
 });
